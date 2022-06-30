e848f004dc6b92f9befb1daff26d8fae1a36038e
SofyaKriv <81295509+SofyaKriv@users.noreply.github.com>
2022-06-30 13:03:24 +0300

    With auth

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth0 } from '@auth0/auth0-react';
 
 import "./Home.css";
 
 function Home () {
+    const {
+        user,
+        isAuthenticated,
+        loginWithRedirect,
+    } = useAuth0();
 
     return(
     <React.Fragment>
         <div>
             <Link to="/videos"><button class="button"><span>Видео</span></button></Link>
             <Link to="/categories"><button class="button"><span>Категории</span></button></Link>
+            {!isAuthenticated ? <button onClick={loginWithRedirect}>Войти</button> : user.name}
             <div className="textHome">VIDEOSHOW</div>
         </div>
     </React.Fragment>
